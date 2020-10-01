62fa2e1869468aa7d0fdb75184b52c7197280123
Bernhard, Dave(AWF) <dbernhard@ebay.com>
2020-10-01 20:48:22 +0100

    style: 4 spaces instead of 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,54 +1,55 @@
 <script>
-  import { gsap } from "gsap";
-  import { ScrollTrigger } from "gsap/ScrollTrigger";
+    import { gsap } from 'gsap'
+    import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-  import { onMount } from "svelte";
+    import { onMount } from 'svelte'
 
-  gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollTrigger)
 
-  export let name;
+    export let name
 
-  let paragraph;
+    let paragraph
 
-  onMount(() => {
-    gsap.to(paragraph, {
-      scrollTrigger: {
-        trigger: paragraph,
-        scrub: true
-      },
-      x: 500,
-      duration: 3
-    });
-  });
+    onMount(() => {
+        gsap.to(paragraph, {
+            scrollTrigger: {
+                trigger: paragraph,
+                scrub: true
+            },
+            x: 500,
+            duration: 3
+        })
+    })
 </script>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 120vh auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
     main {
-      max-width: none;
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 120vh auto;
     }
-  }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
 
 <main>
-  <h1 id="heading">Hello {name}!</h1>
-  <p bind:this={paragraph}>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
+    <section />
+    <h1 id="heading">Hello {name}!</h1>
+    <p bind:this={paragraph}>
+        Visit the
+        <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+        to learn how to build Svelte apps.
+    </p>
 </main>
