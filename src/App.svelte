3407c063e9fbfc62ff24a71a4e1221f0fe572d5e
Dave Bernhard <dave.bernhard@feedagency.com>
2020-10-03 13:10:31 +0100

    wip: create ScrabbleTile component and get separate scrolltriggers working

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,11 @@
 <script>
-    import { gsap } from 'gsap'
-    import { ScrollTrigger } from 'gsap/ScrollTrigger'
-
-    import { onMount } from 'svelte'
-
-    gsap.registerPlugin(ScrollTrigger)
-
-    export let name
-
-    let paragraph
-
-    onMount(() => {
-        gsap.to(paragraph, {
-            scrollTrigger: {
-                trigger: paragraph,
-                scrub: true
-            },
-            x: 500,
-            duration: 3
-        })
-    })
+    import ScrabbleTile from './ScrabbleTile.svelte'
 </script>
 
 <style>
     main {
         text-align: center;
-        padding: 1em;
         max-width: 240px;
-        margin: 120vh auto;
-    }
-
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
     }
 
     @media (min-width: 640px) {
@@ -45,11 +16,34 @@
 </style>
 
 <main>
-    <section />
-    <h1 id="heading">Hello {name}!</h1>
-    <p bind:this={paragraph}>
-        Visit the
-        <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-        to learn how to build Svelte apps.
-    </p>
+    <ScrabbleTile 
+        heading="Aa"
+        definition={`it is a variety of flow lava which has a rough surface of broken blocks. Not surprisingly, the word is Hawaiian, Hawaii having an abundance of both lava and vowels. It is pronounced "ah-ah."`}
+        score="2"
+        dictionary="Collins"
+    />
+    <ScrabbleTile 
+        heading="Ab"
+        definition={`short for "abdominal", innit?`}
+        score="2"
+        dictionary="Collins"
+    />
+    <ScrabbleTile 
+        heading="Ad"
+        definition={`abbreviation of "advertisement"`}
+        score="2"
+        dictionary="Collins"
+    />
+    <ScrabbleTile 
+        heading="Ae"
+        definition={`Scottish for "one", apparently?`}
+        score="2"
+        dictionary="Collins"
+    />
+    <ScrabbleTile 
+        heading="Ag"
+        definition={`bastardized version of "agriculture"`}
+        score="2"
+        dictionary="Collins"
+    />
 </main>
